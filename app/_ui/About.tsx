86014f2a6abdab6ed77ec9c03ddc98db4fd71e02
ngPoi <ngpoi1810@gmail.com>
2024-12-19 00:51:46 +0700

    commit header, about section

---
+++ app/_ui/About.tsx
@@ -0,0 +1,46 @@
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <section
+      className="relative flex min-h-screen w-full items-center justify-center px-[12%] py-32"
+      id="about"
+    >
+      <div className="flex items-center justify-center gap-40">
+        <Image
+          src="/avatar.jpeg"
+          alt="Nguyen Hai Nguyen"
+          width={400}
+          height={400}
+          quality={100}
+          className="w-[30vw] rounded-full"
+        />
+        <div className="flex flex-col items-center justify-between gap-6 text-center">
+          <div>
+            <h3 className="text-3xl font-medium opacity-80">Hi, I&apos;m</h3>
+            <h1 className="my-6 text-6xl font-semibold">Hai Nguyen</h1>
+            <span className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-3xl text-transparent">
+              Frontend Developer
+            </span>
+          </div>
+
+          <div className="flex gap-4">
+            <div className="cursor-pointer text-nowrap rounded-[3rem] border-2 border-solid border-black px-6 py-2 font-medium duration-200 ease-in-out hover:bg-black hover:text-white">
+              Dowload CV
+            </div>
+            <div className="cursor-pointer text-nowrap rounded-[3rem] border-2 border-solid border-black px-6 py-2 font-medium duration-200 ease-in-out hover:bg-black hover:text-white">
+              Contact
+            </div>
+          </div>
+          <div className="flex gap-8">
+            <FontAwesomeIcon icon={faGithub} className="w-8 h-8 cursor-pointer duration-200 ease-in-out hover:scale-110" />
+            <FontAwesomeIcon icon={faLinkedin} className="w-8 h-8 cursor-pointer duration-200 ease-in-out hover:scale-110" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ app/_ui/Header.tsx
@@ -0,0 +1,48 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="fixed left-1/2 top-4 z-50 flex -translate-x-1/2 items-center justify-center gap-20 rounded-[3rem] bg-zinc-700 px-6 py-2 text-white backdrop-blur">
+      <Link
+        href=""
+        className="text-nowrap text-xl font-semibold text-white transition-transform hover:scale-110"
+      >
+        <span>Hai Nguyen</span>
+      </Link>
+      <ul className="flex gap-8">
+        <li>
+          <Link className="line relative font-light text-white" href="#about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="line relative font-light text-white"
+            href="#experience"
+          >
+            Experience
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="line relative font-light text-white"
+            href="#projects"
+          >
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link className="line relative font-light text-white" href="#contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+      <FontAwesomeIcon icon={faBars} className="hidden w-8" />
+      <button className="cursor-pointer text-nowrap rounded-[3rem] border-none bg-gradient-to-r from-purple-500 to-pink-500 px-6 py-3 text-base font-medium text-white transition-all hover:scale-105 hover:bg-gradient-to-r hover:from-pink-500 hover:to-purple-500">
+        Visit Github
+      </button>
+    </header>
+  );
+}
